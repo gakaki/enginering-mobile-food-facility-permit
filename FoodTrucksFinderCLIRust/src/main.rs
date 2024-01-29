8ba981125cc01da463aa29a7a33422331c5f9ba4
gakaki <gakaki@gmail.com>
2024-01-29 18:21:26 +0800

    complete rust cli

--- FoodTrucksFinderCLIRust/src/main.rs
+++ FoodTrucksFinderCLIRust/src/main.rs
@@ -1,106 +1,5 @@
-// mod entity;
-// mod main_test;
-//
-// use std::str::{Bytes, FromStr};
-// use clap::{App, Arg};
-// use crate::entity::FoodTruck::*;
-// use reqwest::{Error, Client};
-//
-// fn parse_csv_to_structs<T: FromStr>(body: &[u8]) -> Result<Vec<T>, Error>
-//     where
-//         T: serde::de::DeserializeOwned,
-//         <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
-// {
-//     let mut reader = csv::Reader::from_reader(body);
-//     let mut records = Vec::new();
-//     for result in reader.deserialize() {
-//         let record: T = result?;
-//         records.push(record);
-//     }
-//     Ok(records)
-// }
-//
-// // Specify that the function returns a Future that resolves to a Result
-// pub async fn request_csv() -> Result<Vec<u8>, Error> {
-//     let client = Client::new();
-//     let response = client.get("https://data.sfgov.org/api/views/rqzj-sfat/rows.csv")
-//         .send().await?;
-//     let body = response.bytes().await?;
-//     Ok(Vec::from(body))
-// }
-// pub async fn fetch_food_trucks() -> Result<Vec<FoodTruck>, reqwest::Error> {
-//     let client = reqwest::Client::new();
-//     let response = client.get("https://data.sfgov.org/api/views/rqzj-sfat/rows.csv")
-//         .send().await?;
-//     let body = response.bytes().await?;
-//     // 使用csv解析库（例如csv-parse）来解析CSV数据
-//     // 然后将每一行解析为FoodTruck结构体
-//     let food_trucks = parse_csv_to_structs::<FoodTruck>(body.as_ref());
-//     match food_trucks {
-//         Ok(food_trucks) => {
-//             println!("{:?}", food_trucks);
-//             // Work with the food_trucks vector
-//         }
-//         Err(e) => eprintln!("Error parsing CSV: {}", e),
-//     }
-//
-//     // Assuming `food_trucks` is a `Vec<Result<FoodTruck, Error>>`
-//     let mut food_trucks: Vec<FoodTruck> = Vec::new();
-//     for truck_result in food_trucks {
-//         match truck_result {
-//             Ok(truck) => food_trucks.push(truck),
-//             Err(e) => {
-//                 // Handle the error appropriately
-//                 println!("Error processing food truck: {}", e);
-//             }
-//         }
-//     }
-//     Ok(food_trucks)
-// }
-// #[tokio::main]
-// async fn main() {
-
-//
-//     let csv_path = matches.value_of("csv_path").unwrap_or("food_trucks.csv");
-//     // 下载CSV数据
-//     let client = reqwest::Client::new();
-//     let response = client.get(&format!("https://data.sfgov.org/api/views/rqzj-sfat/rows.csv"))
-//         .header("Accept", "text/csv")
-//         .send()?;
-//     // 写入CSV数据到文件
-//     let mut file = fs::File::create(csv_path)?;
-//     file.write_all(&response.bytes().unwrap())?;
-//     // 解析CSV数据
-//     let csv_data = csv::Reader::from_path(csv_path)?;
-//     for result in csv_data.deserialize() {
-//         let food_truck: FoodTruck = result?;
-//         // 处理每一个food_truck
-//         println!("{:?}", food_truck);
-//     }
-//
-//     let matches = App::new("CLIFoodTrucksFinderRust")
-//     .version("1.0")
-//     .about("Food trucks finder in San Francisco")
-//     .arg(Arg::with_name("filter_keyword")
-//          .short("k")
-//          .long("keyword")
-//          .takes_value(true)
-//          .help("Filter keyword for LocationID, Applicant, LocationDescription, Address"))
-//     .get_matches();
-//
-// `    // let filter_keyword = matches.value_of("filter_keyword").unwrap_or_else(|| "");
-//     // // 调用fetch_food_trucks获取数据，并过滤
-//     // let food_trucks = fetch_food_trucks().await;
-//     // // 处理并打印过滤后的数据
-//     // match food_trucks {
-//     //     Ok(trucks);
-//     // }
-
-// }
-
-
 mod entity;
-use clap::{App, Arg};
+use clap::{Arg};
 use std::fs;
 use std::path::{Path, PathBuf};
 use crate::entity::food_truck::FoodTruck;
@@ -162,17 +61,36 @@ async fn main() {
             println!("food trucks length is {:?}", food_trucks.len());
 
 
+            // Filter the FoodTrucks based on command-line arguments
             let matches = clap::App::new("filter_food_trucks")
                 .version("1.0")
-                .about("Filter FoodTrucks based on locationid, Applicant, and location_description.")
-                .arg(Arg::with_name("locationid")
-                    .short("l")
-                    .long("locationid")
-                    .help("Filter by locationid")
+                .about("Filter FoodTrucks keyword based on locationid, Applicant, or location_description.")
+                .arg(Arg::new("keyword")
+                    .help("CSV data to filter")
                     .takes_value(true))
                 .get_matches();
 
+            let keyword = matches.value_of("keyword").map(|s| s.to_string());
+            if let Some(keyword) = keyword {
+                if keyword.is_empty() {
 
+                }else{
+                    println!("input args is : {:?}", keyword);
+                    let r = food_trucks
+                        .clone()
+                        .into_iter()
+                        .filter(|item| return
+                            item.location.to_lowercase().contains(&keyword) ||
+                                item.applicant.to_lowercase().contains(&keyword) ||
+                                item.location_description.to_lowercase().contains(&keyword)
+                        ).collect::<Vec<_>>();
+                    println!("filter rows length are : {:?}", r.len());
+
+                    r.iter().for_each(|food_truck: &FoodTruck| {
+                        println!("Value: {}-{}-{}", food_truck.location,food_truck.applicant, food_truck.location_description );
+                    });
+                }
+            }
         }
         Err(e) => eprintln!("Error processing CSV: {}", e),
     }
